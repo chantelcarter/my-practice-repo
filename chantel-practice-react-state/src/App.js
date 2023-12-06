1433ee741a462384831b9fc3582902ae45ecdffa
chantelcarter <chantel.alexia@gmail.com>
2023-12-05 20:53:12 -0800

    react state adding and removing colored squares

---
+++ chantel-practice-react-state/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Buttons from "./components/Buttons"
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+      <Buttons />
+    </>
+  )
+}
+
+export default App
